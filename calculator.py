24c6a6a8beed7052a3a01cf91292a9d6a44fdfb1
ratnadipdoke1712 <ratnadipdoke1712@gmail.com>
2026-02-05 12:09:39 +0530

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,34 @@
+# Simple Calculator with Input Validation
+
+try:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    print("\nChoose an operation:")
+    print("1. Addition (+)")
+    print("2. Subtraction (-)")
+    print("3. Multiplication (*)")
+    print("4. Division (/)")
+
+    choice = input("Enter your choice (1/2/3/4): ")
+
+    if choice == "1":
+        print("Result:", num1 + num2)
+
+    elif choice == "2":
+        print("Result:", num1 - num2)
+
+    elif choice == "3":
+        print("Result:", num1 * num2)
+
+    elif choice == "4":
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Error: Division by zero is not allowed")
+
+    else:
+        print("Invalid choice")
+
+except ValueError:
+    print("Error: Please enter valid numbers only.")
